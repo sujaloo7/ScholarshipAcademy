2a004166f9bff30bd1ecdef1d4919a629fcf9a1d
sujaloo7 <sujal.ktech@gmail.com>
2022-09-21 15:27:55 +0530

    commit

--- src/Repository/Repository.js
+++ src/Repository/Repository.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 // const baseDomain = "https://scholarshipbackend.herokuapp.com/api";
-const baseDomain = "http://localhost:4300/api";
-const imageDomain = "http://localhost:4300";
-// const baseDomain = "https://scholarshipbackend.herokuapp.com/api";
-// const imageDomain = "https://scholarshipbackend.herokuapp.com";
+// const baseDomain = "http://localhost:4300/api";
+// const imageDomain = "http://localhost:4300";
+const baseDomain = "https://scholarshipbackend.herokuapp.com/api";
+const imageDomain = "https://scholarshipbackend.herokuapp.com";
 const authorization_prefix = "Bearer ";
 
 export const customHeaders = {
@@ -28,5 +28,5 @@ export const serializeQuery = (query) => {
         (key) => `${encodeURIComponent(key)}=${encodeURIComponent(query[key])}`
       )
       .join("&");
-  } catch {}
+  } catch { }
 };
